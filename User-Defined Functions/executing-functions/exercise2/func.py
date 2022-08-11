59d37984ce620ac54c65192b9ab2e91cd247d7ed
Orlando McEwan <92196574+omcewan@users.noreply.github.com>
2022-08-11 00:22:02 -0400

    complete exercise 2 func.py

--- User-Defined Functions/executing-functions/exercise2/func.py
+++ User-Defined Functions/executing-functions/exercise2/func.py
@@ -1,7 +1,29 @@
 """  
-A function to roll two dice
+A procedure to roll two dice
 
-Author: YOUR NAME HERE
-Date: THE DATE HERE
+Author: Orlando McEwan
+Date: 08/11/2022
 """
 import random
+
+def rollem(first, last):
+  """
+  Returns the sum of two random numbers.
+
+  The numbers generated are between first and last (inclusive).  
+
+  Example: rollem(1,6) can return any value between 2 and 12.
+
+  Parameter first: The lowest possible number
+  Precondition: first is an integer
+
+  Parameter last: The greatest possible number
+  Precondition: last is an integer, last >= first
+  """
+  
+  num1 = random.randint(first,last)
+  num2 = random.randint(first,last)
+  
+  thesum = num1+num2
+  
+  return thesum
