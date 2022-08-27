e8138c351105ee276efc4bd423a7778256ab5649
Orlando McEwan <92196574+omcewan@users.noreply.github.com>
2022-08-27 13:06:20 -0400

    complete exercise 4, enforcing specifications

--- User-Defined Functions/enforcing-specifications/exercise4/funcs.py
+++ User-Defined Functions/enforcing-specifications/exercise4/funcs.py
@@ -5,8 +5,8 @@ The primary function in this module is str_to_seconds.  The functions get_hours,
 get_minutes, and get_seconds are all helper functions that are used to implement 
 this function.  They should implemented in the order listed.
 
-<YOUR NAME HERE>
-<DATE HERE>
+Author: Orlando McEwan
+Date: 08/27/2022
 """
 import introcs
 
@@ -18,6 +18,9 @@ def iso_8601(s):
     Parameter time: The string to check
     Precondition: s is a string of length 8
     """
+    assert type(s) == str, f'The value {s} is not a string'
+    assert len(s) == 8, f'The length of {s} is not equal to 8'
+    
     # This is a hint to get you started
     # Create variable to check if the first two characters are digits
     check1 = introcs.isdigit(s[:2])
@@ -25,10 +28,13 @@ def iso_8601(s):
     check2 = s[2] == ':'
     
     # FINISH THIS FUNCTION
+    check3 = introcs.isdigit(s[3:5])
+    check4 = s[5] == ':'
+    check5 = introcs.isdigit(s[6:])
     
     # Return True if all checks pass
     #return check1 and check2 # AND...
-
+    return check1 and check2 and check3 and check4 and check5
 
 def get_seconds(time):
     """
@@ -50,8 +56,12 @@ def get_seconds(time):
     
     Parameter time: The string representation of the time
     Precondition: time is a string in extended ISO 8601 format 'hh:mm:ss'
-    """
+    """ 
+    assert type(time) == str, f'The value {time} is not a string!'
+    assert len(time) == 8, f'The length of {time} is not equal to 8!'
+    assert iso_8601(time), f'The time, {time} is not in correct format!'
     
+    return int(time[-2:])
 
 
 def get_minutes(time):
@@ -75,6 +85,11 @@ def get_minutes(time):
     Parameter time: The string representation of the time
     Precondition: time is a string in extended ISO 8601 format 'hh:mm:ss'
     """
+    assert type(time) == str, f'The value {time} is not a string!'
+    assert len(time) == 8, f'The length of {time} is not equal to 8!'
+    assert iso_8601(time), f'The time, {time} is not in correct format!'
+    
+    return int(time[3:5])
     
 
 def get_hours(time):
@@ -98,7 +113,11 @@ def get_hours(time):
     Parameter time: The string representation of the time
     Precondition: time is a string in extended ISO 8601 format 'hh:mm:ss'
     """
-
+    assert type(time) == str, f'The value {time} is not a string!'
+    assert len(time) == 8, f'The length of {time} is not equal to 8!'
+    assert iso_8601(time), f'The time, {time} is not in correct format!'
+    
+    return int(time[0:2])
 
 def str_to_seconds(time):
     """
@@ -109,7 +128,7 @@ def str_to_seconds(time):
     hours, minutes, and seconds, so they are padded with 0s when necessary.  So 
     seconds, minutes, and hours may have leading 0s if they are only one digit. For
     more information, see
-        
+    
         https://en.wikipedia.org/wiki/ISO_8601#Times
     
     This function does not support time zones, abbreviated formats, or decimals
@@ -121,4 +140,13 @@ def str_to_seconds(time):
     Parameter time: The string representation of the time
     Precondition: time is a string in extended ISO 8601 format 'hh:mm:ss'
     """
+    assert type(time) == str, f'The value {time} is not a string!'
+    assert len(time) == 8, f'The length of {time} is not equal to 8!'
+    assert iso_8601(time), f'The time, {time} is not in correct format!'
+    
+    seconds = get_seconds(time)
+    mins_to_seconds = get_minutes(time) * 60
+    hours_to_seconds = get_hours(time) * 60 * 60
+    
+    return seconds + mins_to_seconds + hours_to_seconds
     
