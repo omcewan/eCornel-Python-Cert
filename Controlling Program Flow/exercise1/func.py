58ab3c1cdcb3c3f0455b7d44c307f8a779628a60
Orlando McEwan <92196574+omcewan@users.noreply.github.com>
2022-09-27 18:29:17 -0400

    complete exercise two

--- Controlling Program Flow/exercise1/func.py
+++ Controlling Program Flow/exercise1/func.py
@@ -1,8 +1,8 @@
 """  
 A function to search for the first vowel position
 
-Author: YOUR NAME HERE
-Date: THE DATE HERE
+Author: Orlando McEwan
+Date: 09/26/2022
 """
 import introcs
 
@@ -10,23 +10,23 @@ import introcs
 def first_vowel(s):
     """
     Returns the position of the first vowel in s; it returns len(s) if there are no vowels.
-    
+
     We define the vowels to be the letters 'a','e','i','o', and 'u'.  The letter
     'y' counts as a vowel only if it is not the first letter in the string.
-    
+
     Examples: 
         first_vowel('hat') returns 1
         first_vowel('grrm') returns 4
         first_vowel('sky') returns 2
         first_vowel('year') returns 1
-    
+
     Parameter s: the string to search
     Precondition: s is a nonempty string with only lowercase letters
     """
-    
+
     # set a variable with the smallest index as the length of the string. This will be the starting point and for each vowel we check we will compare its value against the smallest_index variable and we will return this at the end
     result = len(s)
-    
+
     # find the index for every vowel if it exists
     vowel_a = introcs.find_str(s, 'a')
     vowel_e = introcs.find_str(s, 'e')
@@ -34,24 +34,24 @@ def first_vowel(s):
     vowel_o = introcs.find_str(s, 'o')
     vowel_u = introcs.find_str(s, 'u')
     vowel_y = introcs.find_str(s, 'y', 1)
-    
+
     # use if statments to check which vowel has the smallest index.
     if vowel_a < result and vowel_a > -1:
         result = vowel_a
-        
+
     if vowel_e < result and vowel_e > -1:
         result = vowel_e
-        
+
     if vowel_i < result and vowel_i > -1:
         result = vowel_i
-        
+
     if vowel_o < result and vowel_o > -1:
         result = vowel_o
-        
+
     if vowel_u < result and vowel_u > -1:
         result = vowel_u
-        
+
     if vowel_y < result and vowel_y > -1:
         result = vowel_y
-    
+
     return result
--- Controlling Program Flow/exercise2/func.py
+++ Controlling Program Flow/exercise2/func.py
@@ -1,8 +1,8 @@
 """  
 A function to extract names from e-mail addresses.
 
-Author: YOUR NAME HERE
-Date: THE DATE HERE
+Author: Orlando McEwan
+Date: 09/27/2022
 """
 import introcs
 
@@ -10,26 +10,33 @@ import introcs
 def extract_name(s):
     """
     Returns the first name of the person in e-mail address s.
-    
+
     We assume (see the precondition below) that the e-mail address is in one of
     two forms:
-        
+
         last.first@megacorp.com
         first.last@mompop.net
-    
+
     where first and last correspond to the person's first and last name.  Names
     are not empty, and contain only letters. Everything after the @ is guaranteed 
     to be exactly as shown.
-    
+
     The function preserves the capitalization of the e-mail address.
-    
+
     Examples: 
         extract_name('smith.john@megacorp.com') returns 'john'
         extract_name('maggie.white@mompop.net') returns 'maggie'
         extract_name('Bob.Bird@mompop.net') returns 'Bob'
-    
+
     Parameter s: The e-mail address to extract from
     Precondition: s is in one of the two address formats described above
     """
     # You must use an if-else statement in this function.
-    pass
+    email_end = s[-3:]
+
+    if email_end == 'net':
+        # print(s[:introcs.index_str(s, '.')])
+        return s[:introcs.index_str(s, '.')]
+    else:
+        # print(s[introcs.index_str(s, '.') + 1: introcs.index_str(s, '@')])
+        return s[introcs.index_str(s, '.') + 1: introcs.index_str(s, '@')]
