1d9d476076f4dec47664d5db13591a43a88de1da
Orlando McEwan <92196574+omcewan@users.noreply.github.com>
2022-09-27 19:05:22 -0400

    completed exercise 3

--- Controlling Program Flow/exercise3/func.py
+++ Controlling Program Flow/exercise3/func.py
@@ -1,8 +1,8 @@
 """  
 A function to extract names from e-mail addresses.
 
-Author: YOUR NAME HERE
-Date: THE DATE HERE
+Author: Orlando McEwan
+Date: 09/27/2022
 """
 import introcs
 
@@ -10,28 +10,35 @@ import introcs
 def extract_name(s):
     """
     Returns the first name of the person in e-mail address s.
-    
+
     We assume (see the precondition below) that the e-mail address is in one of
     three forms:
-        
+
         last.first@megacorp.com
         last.first.middle@consultant.biz
         first.last@mompop.net
-    
+
     where first, last, and middle correspond to the person's first, middle, and
     last name. Names are not empty, and contain only letters. Everything after the 
     @ is guaranteed to be exactly as shown.
-    
+
     The function preserves the capitalization of the e-mail address.
-    
+
     Examples: 
         extract_name('smith.john@megacorp.com') returns 'john'
         extract_name('McDougal.Raymond.Clay@consultant.biz') returns 'Raymond'
         extract_name('maggie.white@mompop.net') returns 'maggie'
         extract_name('Bob.Bird@mompop.net') returns 'Bob'
-    
+
     Parameter s: The e-mail address to extract from
     Precondition: s is in one of the two address formats described above
     """
     # You must use an if-elif-else statement in this function.
-    pass
+    if s[-3:] == 'com':
+        return s[introcs.index_str(s, '.') + 1: introcs.index_str(s, '@')]
+    elif s[-3:] == 'net':
+        return s[:introcs.index_str(s, '.')]
+    else:
+        first_dot = introcs.index_str(s, '.')
+        second_dot = introcs.index_str(s, '.', first_dot + 1,)
+        return s[first_dot + 1: second_dot]
