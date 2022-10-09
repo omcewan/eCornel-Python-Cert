faeb7e4140d6a77f95430033a50f8ea463e3b590
Orlando McEwan <92196574+omcewan@users.noreply.github.com>
2022-10-09 12:52:26 -0400

    completed exercise two for try except statements

--- Controlling Program Flow/Using Try-Exempt Statements/exercise2/func.py
+++ Controlling Program Flow/Using Try-Exempt Statements/exercise2/func.py
@@ -1,8 +1,8 @@
 """  
 A function to test for floats in European format
 
-Author: YOUR NAME HERE
-Date: THE DATE HERE
+Author: Orlando McEwan
+Date: 10/09/2022
 """
 import introcs
 
@@ -10,17 +10,17 @@ import introcs
 def iseurofloat(s):
     """
     Returns True if s is a float in European format.  Returns False otherwise.
-    
+
     In European format, a comma is used in place of a decimal point.  So '12,5' stands
     for 12.5, '0,12' stands for 0.12 and so.  Formally, a string is in European format
     if it is of the form <d1>,<d2> where d1 and d2 are ints (and d2 >= 0).  See
-        
+
         https://en.wikipedia.org/wiki/Decimal_separator
-    
+
     for more information.
-    
+
     This function does not recognize floats in scientific notation (e.g. '1e-2').
-    
+
     Examples: 
         iseurofloat('12,5') returns True
         iseurofloat('-12,5') returns True
@@ -31,9 +31,18 @@ def iseurofloat(s):
         iseurofloat('12,5.3') returns False
         iseurofloat('12,5,3') returns False
         iseurofloat('1e-2') returns False
-    
+
     Parameter s: The string to check
     Precondition: s is a string
     """
     # You MAY NOT use conditionals anywhere in this function.
-    pass
+
+    try:
+        comma1 = introcs.index_str(s, ',')
+        int_before_comma = int(s[:comma1])
+        int_after_comma = int(s[comma1+1:])
+
+        minus = introcs.find_str(s, '-')
+        return minus < comma1
+    except:
+        return False
